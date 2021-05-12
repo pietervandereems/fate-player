37bb6bb27099912978a396cba3c7740a37d013c0
Pieter van der Eems <gpggit@eemco.nl>
2021-05-12 20:42:10 +0200

    Simple placeholder components

---
+++ src/Components/CharacterList.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CharacterList = () => {
+  return (
+    <>
+      <h2>CharacterList</h2>
+      Hello, Welcome to the CharacterList
+    </>
+  )
+}
+
+export default CharacterList
---
+++ src/Components/Overview.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Overview = () => {
+  return (
+    <>
+      <h2>Overview</h2>
+      Hello, Welcome to the overview
+    </>
+  )
+}
+
+export default Overview
