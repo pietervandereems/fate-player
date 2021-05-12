824c9c5d452c4eef61c032c2ba0cf0d1bd275d66
Pieter van der Eems <gpggit@eemco.nl>
2021-05-12 20:41:20 +0200

    Very simple notification component with reducer

---
+++ src/Components/Notfication.component.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const Notification = () => {
+  const { txt, full, isError } = useSelector(state => state.notification)
+  if (!txt) {
+    return null
+  }
+
+  const defaultStyle = {
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+
+  const errorStyle = {
+    ...defaultStyle,
+    color: 'red'
+  }
+
+  const noteStyle = {
+    ...defaultStyle,
+    color: 'darkgreen'
+  }
+
+  return (
+    <div id='notification' style={isError ? errorStyle : noteStyle}>
+      {txt}
+      {full ? <p>{full}</p> : null}
+    </div>
+  )
+}
+
+export default Notification
