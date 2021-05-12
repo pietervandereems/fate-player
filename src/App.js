1f85f40a64d084f59fc57b458a2e8cb930a272f7
Pieter van der Eems <gpggit@eemco.nl>
2021-05-12 20:50:17 +0200

    App.js with a basic structure with db connection and user login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg'
-import './App.css'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Notification from './Components/Notfication.component'
+import Login from './Components/Login.component'
+import Overview from './Components/Overview.component'
+import { initialLogin, logout } from './Reducers/user.reducer'
+import {
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+import { Button, Nav } from './Components/Styles'
+import { connect } from './Services/data.service'
+import CharacterList from './Components/CharacterList.component'
 
-function App () {
+const App = () => {
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.user)
+
+  useEffect(() => {
+    dispatch(initialLogin())
+  }, [dispatch])
+
+  useEffect(() => {
+    dispatch(connect(user))
+  }, [user, dispatch])
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+    dispatch(logout())
+  }
+
+  const padding = { padding: 5 }
+
   return (
     <>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav>
+        <section>
+          <Link style={padding} to='/'>App</Link>
+          <Link style={padding} to='/Characters'>Characters</Link>
+        </section>
+        {user
+          ? <em>{user.name} logged in <Button onClick={handleLogout}>logout</Button></em>
+          : null}
+      </Nav>
+      <Notification />
+      <h1>Fate-Player</h1>
+      {user
+        ? (
+          <section>
+            <Switch>
+              <Route path='/Characters'>
+                <CharacterList />
+              </Route>
+              <Route path='/'>
+                <Overview />
+              </Route>
+            </Switch>
+          </section>
+          )
+        : <Login />}
+
     </>
   )
 }
