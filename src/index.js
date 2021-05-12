2709511b7af5cd712911e81629e806150be859c2
Pieter van der Eems <gpggit@eemco.nl>
2021-05-12 20:38:48 +0200

    Add the router and redux to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { Provider } from 'react-redux'
 import App from './App'
+import store from './Utils/Store'
+import {
+  BrowserRouter as Router
+} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
