90af6d1790659d1f99e80f670385c3a8b79d1a3d
Pieter van der Eems <gpggit@eemco.nl>
2021-05-13 12:47:28 +0200

    Show a list of player characters and the games these characters are in

--- src/Components/CharacterList.component.js
+++ src/Components/CharacterList.component.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const CharacterList = () => {
+  const characters = useSelector(state => state.characters)
+  const games = useSelector(state => state.games)
+
+  const gameNames = (linkedGames) => linkedGames
+    .map((gameId) => games.find((game) => game._id === gameId)?.name ?? 'Unknown')
+    .join(',')
+
   return (
     <>
       <h2>CharacterList</h2>
-      Hello, Welcome to the CharacterList
+      Hello, These are your available characters
+      <ul>
+        {characters.map(character => <li key={character._id}>{character.name} ({gameNames(character.games)})</li>)}
+      </ul>
     </>
   )
 }
