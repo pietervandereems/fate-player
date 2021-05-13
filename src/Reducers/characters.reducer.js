f9f7a75bbb2104bc013d344af6b4fc3b21563221
Pieter van der Eems <gpggit@eemco.nl>
2021-05-13 12:45:41 +0200

    Use default type reducers

--- src/Reducers/characters.reducer.js
+++ src/Reducers/characters.reducer.js
@@ -1,22 +1,15 @@
-const rev = (doc) => parseInt(doc._rev.split('_')[0], 10)
+import {
+  updateDB,
+  dbUpdateReducer,
+  initializerReducer
+} from './types.reducer'
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case 'DBUPDATE': {
-      const newVersion = action.data
-      const prevVersion = state.find((doc) => doc._id === newVersion._id)
-      if (prevVersion == null) {
-        return [
-          ...state,
-          newVersion
-        ]
-      }
-      if (rev(newVersion) > rev(prevVersion)) {
-        return state.map((doc) => (doc._id === newVersion._id) ? newVersion : doc)
-      }
-      return state
+    case 'DBUPDATECHARACTER': {
+      return updateDB({ state, action })
     }
-    case 'INITIALIZE': {
+    case 'INITIALIZECHARACTER': {
       return action.data
     }
     default:
@@ -24,18 +17,7 @@ const reducer = (state = [], action) => {
   }
 }
 
-export const dbUpdate = (doc) => dispatch => {
-  dispatch({
-    type: 'DBUPDATE',
-    data: doc
-  })
-}
-
-export const initialize = (docs) => dispatch => {
-  dispatch({
-    type: 'INITIALIZE',
-    data: docs.filter((doc) => doc.type === 'character')
-  })
-}
+export const dbUpdate = dbUpdateReducer('DBUPDATECHARACTER')
+export const initialize = initializerReducer({ type: 'INITIALIZECHARACTER', docType: 'character' })
 
 export default reducer
